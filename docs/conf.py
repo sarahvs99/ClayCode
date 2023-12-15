9b1e20d07628be39df407829b04f7b5f79f04ffb
Sarah Stewart <sarahvs448@gmail.com>
2023-12-15 15:11:03 +0000

    Adding API

--- docs/conf.py
+++ docs/conf.py
@@ -25,6 +25,10 @@ release = '0.1.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))  # Source code dir relative to this file
+
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones
 extensions = [
